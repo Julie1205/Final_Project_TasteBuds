95645facb6ec1d54db7c41f85fb69cc72fdb9700
Julie Hoang <juliehoang12@hotmail.com>
2022-09-29 13:04:50 -0400

    BE get restaurants near me endpoint done

---
+++ server/mongoDbConstants.js
@@ -0,0 +1,4 @@
+const DATABASE_NAME = "db-name";
+const USERS_COLLECTION = "users";
+
+module.exports = { DATABASE_NAME, USERS_COLLECTION };
--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const { getRestaurantsNearMe } = require("./handlers");
 const app = express();
 const port = 8000;
 
 app.use(morgan("tiny"));
 app.use(express.json());
 
-app.get("/api/hello", (req, res) => {
-    res.status(200).json({status: 200, message: "hi"});
-});
+//test endpoint
+// app.get("/api/hello", (req, res) => {
+//     res.status(200).json({status: 200, message: "hi"});
+// });
+
+//get restaurants near me endpoint
+app.get("/get-restaurants-near-me/:address", getRestaurantsNearMe);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
